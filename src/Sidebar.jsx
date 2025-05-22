0f54ed5049c2cc589cbd488c50caed7534ffc84c
Shisir <shisiraryal555@gmail.com>
2025-05-22 16:13:19 +0545

    first

---
+++ src/Sidebar.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const Sidebar = () => {
+   return (
+     <div classsName = "text-red-600 w-[50%]c">
+       
+     </div> 
+   )
+ }
+ 
+ export default Sidebar
+ 
---
+++ src/component/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className = 'flex justify-between mx-5'>
+        <div>log</div>
+        <div>nav</div>
+        <div>auth</div>
+    </div>
+  )
+}
+
+export default header
